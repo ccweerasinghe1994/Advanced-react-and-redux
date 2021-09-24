046d2cf42a73fb3aaf9435889f50588bb5317042
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-09-25 04:45:00 +0530

    first test is initiated

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from "@testing-library/react";
+import React from "react-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+  const div = document.createElement("div");
+  React.render(<App />, div);
+  expect(div.innerHTML).toContain("Hi There!");
 });
